771abfb4ff4673cd90e6eacde29499cd2dd11cf0
zhangph1220 <zhangph1220@gmail.com>
2019-01-30 23:17:38 -0500

    add profile model to DB

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route
+// Load Profile model
+const Profile = require('../../models/Profile');
+// Load User model
+const User = require('../../models/User');
+
+// @route   GET api/profile/test
+// @desc    Tests profile router
+// @access  Public
 router.get('/test', (req, res) => res.json({
     msg: "Profile Works"
 }));
+
+// @route   GET api/profile
+// @desc    Get current user's profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({
+            // we parse the authorization, thus we use 'user' parse
+            user: req.user.id
+        })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
